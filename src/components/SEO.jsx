da25bfa06a40fb5ab21847a1eb328dec55e75f35
ayushmishra7597@gmail.com <ayushmishra@gmail.com>
2026-02-25 16:02:35 +0530

    nothing to commit

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,12 +1,14 @@
 
 import {Helmet} from "react-helmet"
 export default function SEO({ title, description, canonical }) {
+  
   const cleanCanonical = canonical?.replace(/\/+$/, "");
+  
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="canonical" href={cleanCanonical} />
+      <link rel="canonical" href={cleanCanonical} key="canonical"/>
     
     </Helmet>
   );
