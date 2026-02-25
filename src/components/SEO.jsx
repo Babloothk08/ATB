340fa88e68aeb751ee5f2641d1900bc6675427ee
ayushmishra7597@gmail.com <ayushmishra@gmail.com>
2026-02-25 15:06:03 +0530

    fixing seo problem

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,16 +1,13 @@
 
-
 import {Helmet} from "react-helmet"
 export default function SEO({ title, description, canonical }) {
+  console.log(title,canonical)
   return (
-    <Helmet >
+    <Helmet>
       <title>{title}</title>
-
       <meta name="description" content={description} />
-
-      {canonical && (
-        <link rel="canonical" href={canonical} />
-      )}
+      <link rel="canonical" href={canonical} />
+    
     </Helmet>
   );
 }
